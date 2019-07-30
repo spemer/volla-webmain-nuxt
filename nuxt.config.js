4b8509cb5a944b7eee85ed6b89bf8de77f985171
Unna Kim <work@unnakim.info>
2019-07-30 17:32:32 +0900

    [config] serverless deploy config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: 'nuxt ashnamuh lambda',
+    title: 'Volla',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: 'deplying nuxt to lambda example by ashnamuh'
+        content: 'Volla'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
