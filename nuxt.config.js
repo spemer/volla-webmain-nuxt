9909b6fb176a75b68ee1382b565ad22ee74e4117
Unna Kim <work@unnakim.info>
2019-07-30 17:17:47 +0900

    [config] sls domain config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,37 +4,40 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'nuxt ashnamuh lambda',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'deplying nuxt to lambda example by ashnamuh'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
+  performance: {
+    gzip: false
+  },
+  router: {
+    base: '/dev'
+  },
+
   /*
    ** Customize the progress-bar color
    */
   loading: { color: '#fff' },
+
   /*
    ** Global CSS
    */
   css: [],
+
   /*
    ** Plugins to load before mounting the App
    */
   plugins: [],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  devModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
-  ],
+
   /*
    ** Nuxt.js modules
    */
@@ -43,17 +46,13 @@ module.exports = {
     '@nuxtjs/axios',
     '@nuxtjs/pwa'
   ],
-  performance: {
-    gzip: false
-  },
-  router: {
-    base: '/dev'
-  },
   /*
    ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
    */
-  axios: {},
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
   /*
    ** Build configuration
    */
@@ -61,6 +60,16 @@ module.exports = {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
   }
 }
